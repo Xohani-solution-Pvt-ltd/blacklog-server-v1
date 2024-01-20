84eb4f68c1ede211049ccb2a306ffff14700eca7
himanshuxohani <146070210+himanshuxohani@users.noreply.github.com>
2024-01-20 11:42:28 +0530

    Revert "db update"

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,18 +1,7 @@
-// const mongoose = require('mongoose');
-
-// mongoose.connect("mongodb://localhost:27017/gyroDB").then(() => {
-//     console.log("connection successfully");
-// }).catch((e) => {
-//     console.log("No connection");
-// });
-
 const mongoose = require('mongoose');
 
-// Replace this connection string with the actual connection string for your MongoDB database
-const dbConnectionString = "mongodb://localhost:27017/gyroDB";
-
-mongoose.connect(dbConnectionString, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
-    console.log("Connection successful");
+mongoose.connect("mongodb://localhost:27017/gyroDB").then(() => {
+    console.log("connection successfully");
 }).catch((e) => {
-    console.log("No connection", e.message);
+    console.log(" No connection ");
 });
