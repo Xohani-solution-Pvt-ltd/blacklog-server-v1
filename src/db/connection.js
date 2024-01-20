8e67e3bf60aa14e421ea077d7d807545304c95f7
Himanshu <himanshu.m@xohani.com>
2024-01-20 11:29:55 +0530

    db update

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,7 +1,18 @@
+// const mongoose = require('mongoose');
+
+// mongoose.connect("mongodb://localhost:27017/gyroDB").then(() => {
+//     console.log("connection successfully");
+// }).catch((e) => {
+//     console.log("No connection");
+// });
+
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost:27017/gyroDB").then(() => {
-    console.log("connection successfully");
+// Replace this connection string with the actual connection string for your MongoDB database
+const dbConnectionString = "mongodb://localhost:27017/gyroDB";
+
+mongoose.connect(dbConnectionString, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+    console.log("Connection successful");
 }).catch((e) => {
-    console.log(" No connection ");
+    console.log("No connection", e.message);
 });
