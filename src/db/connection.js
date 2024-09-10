47631e2f4149c8b095b46d8968cd7d7ccda3a4d7
Himanshu <himanshu.m@xohani.com>
2024-09-10 12:58:39 +0530

    db update

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost:27017/gyroDB").then(() => {
+// mongoose.connect("mongodb://localhost:27017/gyroDB").then(() => {
+mongoose.connect("mongodb+srv://himanshumankar22:blacklog@cluster0.uo1cm.mongodb.net/gyroDB?retryWrites=true&w=majority&appName=Cluster0").then(() => {
     console.log("connection successfully");
 }).catch((e) => {
     console.log(" No connection ");
