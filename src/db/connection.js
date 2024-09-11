632791cfa1f2f5fbb09d1266bd0f1049ef23aa74
Himanshu <himanshu.m@xohani.com>
2024-09-11 13:21:09 +0530

    ..

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,27 +1,9 @@
-// const mongoose = require('mongoose');
-
-// mongoose.connect("mongodb://localhost:27017/gyroDB").then(() => {
-//     console.log("connection successfully");
-// }).catch((e) => {
-//     console.log(" No connection ");
-// });
-
-// import mongoose, { Mongoose } from "mongoose";
 const mongoose = require('mongoose');
 
+mongoose.connect("mongodb://13.232.113.131/gyroDB").then(() => {
+    console.log("connection successfully");
+}).catch((e) => {
+    console.log(" No connection ");
+});
 
-const { MONGO_URI } = process.env;
-
-if (!MONGO_URI) throw new Error("MONGO_URI is not defined.");
-
-let cached = global.mongoose;
-
-if (!cached) cached = global.mongoose = { conn: null };
-
-// export const connectMongo = async () => {
-//     if (cached.conn) return cached.conn;
-
-//     cached.conn = await mongoose.connect(MONGO_URI);
-
-//     return cached.conn;
-// };
+import mongoose, { Mongoose } from "mongoose";
