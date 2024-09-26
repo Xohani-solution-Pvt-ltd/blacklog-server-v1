26658b2ffac26967c16ffce52e84cdc8ef2755ce
Himanshu <himanshu.m@xohani.com>
2024-09-26 13:28:54 +0530

    connected to db atlas

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,9 +1,20 @@
+// const mongoose = require('mongoose');
+
+// mongoose.connect("mongodb://localhost:27017/gyroDB").then(() => {
+//     console.log("connection successfull");
+// }).catch((e) => {
+//     console.log("No connection");
+// });
+
+
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost:27017/gyroDB").then(() => {
-    console.log("connection successfull");
-}).catch((e) => {
-    console.log("No connection");
+mongoose.connect("mongodb+srv://himanshumankar22:blacklog@cluster0.uo1cm.mongodb.net/gyroDB?retryWrites=true&w=majority&appName=Cluster0", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log("Connected to MongoDB Atlas");
+}).catch((error) => {
+  console.error("Error connecting to MongoDB:", error);
 });
-
 
