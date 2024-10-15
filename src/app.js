b3cc1e3b26ef98feb4438db6321f26bc268730c7
Himanshu <himanshu.m@xohani.com>
2024-10-15 11:09:32 +0530

    remove ws

--- src/app.js
+++ src/app.js
@@ -1,42 +1,3 @@
-// const express = require("express");
-// const cookieParser = require('cookie-parser');
-// require('dotenv').config({ path: ".env" });
-// require("../src/db/connection");
-// const router = require("./router/router");
-// const userRouter = require('./router/userRoute');
-// const editRouter = require('./router/editRouter');
-// const testRouter = require('./router/testRoute');
-// const carRouter = require('./router/carRoute');
-// const vehicleRoute = require('./router/vehicleRoute');
-
-// var cors = require('cors')
-
-// const hostname = process.env.HOSTNAME;
-// const port = process.env.PORT || 3000;
-// const app = express();
-
-// app.use(cors({ origin: '*' }))
-// app.use(express.json());
-// app.use("/api/v1/", router);
-// app.use("/api/v1/", userRouter);
-// app.use("/api/v1/", editRouter);
-// app.use("/api/v1/", testRouter);
-// app.use("/api/v1/", carRouter);
-// app.use("/api/v1", vehicleRoute);
-
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.static("public"));
-// app.use(cookieParser());
-
-
-// // app.listen(port, () => {
-// //     console.log(`connection is live at host and port :${hostname}:${port}`);
-// // })
-
-// app.listen(3000, '0.0.0.0', () => {
-//     console.log('Server running on port 3000');
-// });
-
 const express = require("express");
 const cookieParser = require('cookie-parser');
 require('dotenv').config({ path: ".env" });
@@ -48,22 +9,14 @@ const testRouter = require('./router/testRoute');
 const carRouter = require('./router/carRoute');
 const vehicleRoute = require('./router/vehicleRoute');
 
-const cors = require('cors');
-const http = require('http'); // Import http module
-const WebSocket = require('ws'); // Import WebSocket library
+var cors = require('cors')
 
 const hostname = process.env.HOSTNAME;
 const port = process.env.PORT || 3000;
 const app = express();
 
-// Middleware
-app.use(cors({ origin: '*' }));
+app.use(cors({ origin: '*' }))
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
-app.use(cookieParser());
-
-// API Routes
 app.use("/api/v1/", router);
 app.use("/api/v1/", userRouter);
 app.use("/api/v1/", editRouter);
@@ -71,40 +24,90 @@ app.use("/api/v1/", testRouter);
 app.use("/api/v1/", carRouter);
 app.use("/api/v1", vehicleRoute);
 
-// Create HTTP server
-const server = http.createServer(app);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(cookieParser());
 
-// Create WebSocket server
-const wss = new WebSocket.Server({ server });
 
-// Handle WebSocket connections
-wss.on('connection', (ws) => {
-    console.log('New client connected');
+// app.listen(port, () => {
+//     console.log(`connection is live at host and port :${hostname}:${port}`);
+// })
 
-    ws.on('message', async (message) => {
-        const data = JSON.parse(message);
-
-        // Assuming you have a gyroModel defined somewhere
-        const gyroPostData = new gyroModel(data);
-
-        try {
-            await gyroPostData.save();
-            ws.send(JSON.stringify({ message: "GyroData Added Successfully" }));
-        } catch (error) {
-            console.error(error);
-            ws.send(JSON.stringify({ message: "Error saving data", error: error.message }));
-        }
-    });
-
-    ws.on('close', () => {
-        console.log('Client disconnected');
-    });
-});
-
-// Start the server
-// server.listen(port, '0.0.0.0', () => {
-//     console.log(`Server running on http://${hostname}:${port}`);
-// });
 app.listen(3000, '0.0.0.0', () => {
     console.log('Server running on port 3000');
 });
+
+// const express = require("express");
+// const cookieParser = require('cookie-parser');
+// require('dotenv').config({ path: ".env" });
+// require("../src/db/connection");
+// const router = require("./router/router");
+// const userRouter = require('./router/userRoute');
+// const editRouter = require('./router/editRouter');
+// const testRouter = require('./router/testRoute');
+// const carRouter = require('./router/carRoute');
+// const vehicleRoute = require('./router/vehicleRoute');
+
+// const cors = require('cors');
+// const http = require('http'); // Import http module
+// const WebSocket = require('ws'); // Import WebSocket library
+
+// const hostname = process.env.HOSTNAME;
+// const port = process.env.PORT || 3000;
+// const app = express();
+
+// // Middleware
+// app.use(cors({ origin: '*' }));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.static("public"));
+// app.use(cookieParser());
+
+// // API Routes
+// app.use("/api/v1/", router);
+// app.use("/api/v1/", userRouter);
+// app.use("/api/v1/", editRouter);
+// app.use("/api/v1/", testRouter);
+// app.use("/api/v1/", carRouter);
+// app.use("/api/v1", vehicleRoute);
+
+// // Create HTTP server
+// const server = http.createServer(app);
+
+// // Create WebSocket server
+// const wss = new WebSocket.Server({ server });
+
+// // Handle WebSocket connections
+// wss.on('connection', (ws) => {
+//     console.log('New client connected');
+
+//     ws.on('message', async (message) => {
+//         const data = JSON.parse(message);
+
+//         // Assuming you have a gyroModel defined somewhere
+//         const gyroPostData = new gyroModel(data);
+
+//         try {
+//             await gyroPostData.save();
+//             ws.send(JSON.stringify({ message: "GyroData Added Successfully" }));
+//         } catch (error) {
+//             console.error(error);
+//             ws.send(JSON.stringify({ message: "Error saving data", error: error.message }));
+//         }
+//     });
+
+//     ws.on('close', () => {
+//         console.log('Client disconnected');
+//     });
+// });
+
+// // Start the server
+// // server.listen(port, '0.0.0.0', () => {
+// //     console.log(`Server running on http://${hostname}:${port}`);
+// // });
+// // app.listen(3000, '0.0.0.0', () => {
+// //     console.log('Server running on port 3000');
+// // }); 
+// server.listen(port, '0.0.0.0', () => {
+//     console.log(`Server running on http://${hostname}:${port}`);
+// });
--- src/router/router.js
+++ src/router/router.js
@@ -1,84 +1,8 @@
-// const router = require("express").Router();
-// const gyroModel = require("../models/gyro");
-// const carModel = require("../models/car");
-
-// //POST REQ
-// router.post("/addGyro", async (req, res) => {
-//     try {
-//         const { vehicleNo, GPSfix, Date, Time, Latitude, LatitudeDirection, Longitude, LongitudeDirection, Speed, Heading, NoOfSatellites, Altitude, PDOP, HDOP, NetworkOperatorName, Ignition, MainPowerStatus, MainInputVoltage, EmergencyStatus, GSMSignalStrength, MCC, MNC, LAC, CellId, NMR, DigitalInputStatus, DigitalOutputStatus, acceloX, acceloY, acceloZ, gyroX, gyroY, gyroZ, temperature } = req.body;
-
-//         const carData = await carModel.findOne({ vehicleNo });
-
-//         const gyroPostData = new gyroModel({
-//             vehicleNo, GPSfix, Date, Time, Latitude, LatitudeDirection, Longitude, LongitudeDirection, Speed, Heading, NoOfSatellites, Altitude, PDOP, HDOP, NetworkOperatorName, Ignition, MainPowerStatus, MainInputVoltage, EmergencyStatus, GSMSignalStrength, MCC, MNC, LAC, CellId, NMR, DigitalInputStatus, DigitalOutputStatus, acceloX, acceloY, acceloZ, gyroX, gyroY, gyroZ, temperature,
-//             car: carData ? carData._id : null
-//         });
-
-//         await gyroPostData.save().then(() => {
-//             res.status(200).json({ message: "GyroData Added Successfully" });
-//         })
-//     } catch (error) {
-//         console.log(error);
-//     }
-// })
-
-// // GET REQ
-// router.get("/fetchGyro", async (req, res) => {
-//     try {
-//         const data = await gyroModel.find();
-//         res.status(200).json({ data });
-//     } catch (error) {
-//         console.log(error);
-//     }
-// })
-
-
-// //GET REQ WITH ID
-// router.get("/fetchGyro/id", async (req, res) => {
-//     let data;
-//     const id = req.query.id;
-//     try {
-//         data = await gyroModel.findById(id);
-//         res.status(200).json({ data });
-
-//     } catch (error) {
-//         console.log(error);
-//     }
-// })
-
-// //UPDATE BOOK BY ID
-// router.patch("/updateGyro/id", async (req, res) => {
-//     const id = req.query.id;
-//     let data;
-//     try {
-//         data = await gyroModel.findByIdAndUpdate(id, req.body);
-//         await data.save()
-//             .then(() =>
-//                 res.json({ message: "Data Updated Successfully" }))
-
-//     } catch (error) {
-//         console.log(error);
-//     }
-// })
-
-// // DELETE BOOK BY ID 
-// router.delete("/deleteGyro/id", async (req, res) => {
-//     const id = req.query.id;
-//     try {
-//         const data = await gyroModel.findByIdAndDelete(id)
-//             .then(() => res.status(201).json({ message: "Data Deleted Successfully" }))
-//     } catch (error) {
-//         console.log(error);
-//     }
-// })
-
-// module.exports = router;
-
 const router = require("express").Router();
 const gyroModel = require("../models/gyro");
 const carModel = require("../models/car");
 
-// POST REQ
+//POST REQ
 router.post("/addGyro", async (req, res) => {
     try {
         const { vehicleNo, GPSfix, Date, Time, Latitude, LatitudeDirection, Longitude, LongitudeDirection, Speed, Heading, NoOfSatellites, Altitude, PDOP, HDOP, NetworkOperatorName, Ignition, MainPowerStatus, MainInputVoltage, EmergencyStatus, GSMSignalStrength, MCC, MNC, LAC, CellId, NMR, DigitalInputStatus, DigitalOutputStatus, acceloX, acceloY, acceloZ, gyroX, gyroY, gyroZ, temperature } = req.body;
@@ -86,50 +10,17 @@ router.post("/addGyro", async (req, res) => {
         const carData = await carModel.findOne({ vehicleNo });
 
         const gyroPostData = new gyroModel({
-            vehicleNo,
-            GPSfix,
-            Date,
-            Time,
-            Latitude,
-            LatitudeDirection,
-            Longitude,
-            LongitudeDirection,
-            Speed,
-            Heading,
-            NoOfSatellites,
-            Altitude,
-            PDOP,
-            HDOP,
-            NetworkOperatorName,
-            Ignition,
-            MainPowerStatus,
-            MainInputVoltage,
-            EmergencyStatus,
-            GSMSignalStrength,
-            MCC,
-            MNC,
-            LAC,
-            CellId,
-            NMR,
-            DigitalInputStatus,
-            DigitalOutputStatus,
-            acceloX,
-            acceloY,
-            acceloZ,
-            gyroX,
-            gyroY,
-            gyroZ,
-            temperature,
+            vehicleNo, GPSfix, Date, Time, Latitude, LatitudeDirection, Longitude, LongitudeDirection, Speed, Heading, NoOfSatellites, Altitude, PDOP, HDOP, NetworkOperatorName, Ignition, MainPowerStatus, MainInputVoltage, EmergencyStatus, GSMSignalStrength, MCC, MNC, LAC, CellId, NMR, DigitalInputStatus, DigitalOutputStatus, acceloX, acceloY, acceloZ, gyroX, gyroY, gyroZ, temperature,
             car: carData ? carData._id : null
         });
 
-        await gyroPostData.save();
-        res.status(200).json({ message: "GyroData Added Successfully" });
+        await gyroPostData.save().then(() => {
+            res.status(200).json({ message: "GyroData Added Successfully" });
+        })
     } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: "Error adding GyroData", error: error.message });
+        console.log(error);
     }
-});
+})
 
 // GET REQ
 router.get("/fetchGyro", async (req, res) => {
@@ -137,78 +28,187 @@ router.get("/fetchGyro", async (req, res) => {
         const data = await gyroModel.find();
         res.status(200).json({ data });
     } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: "Error fetching GyroData", error: error.message });
+        console.log(error);
     }
-});
+})
 
-// GET REQ WITH ID
+
+//GET REQ WITH ID
 router.get("/fetchGyro/id", async (req, res) => {
+    let data;
     const id = req.query.id;
     try {
-        const data = await gyroModel.findById(id);
-        if (!data) {
-            return res.status(404).json({ message: "GyroData not found" });
-        }
+        data = await gyroModel.findById(id);
         res.status(200).json({ data });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: "Error fetching GyroData by ID", error: error.message });
-    }
-});
 
-// UPDATE GYRO BY ID
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+//UPDATE BOOK BY ID
 router.patch("/updateGyro/id", async (req, res) => {
     const id = req.query.id;
+    let data;
     try {
-        const data = await gyroModel.findByIdAndUpdate(id, req.body);
-        if (!data) {
-            return res.status(404).json({ message: "GyroData not found" });
-        }
-        res.json({ message: "Data Updated Successfully" });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: "Error updating GyroData", error: error.message });
-    }
-});
+        data = await gyroModel.findByIdAndUpdate(id, req.body);
+        await data.save()
+            .then(() =>
+                res.json({ message: "Data Updated Successfully" }))
 
-// DELETE GYRO BY ID 
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+// DELETE BOOK BY ID 
 router.delete("/deleteGyro/id", async (req, res) => {
     const id = req.query.id;
     try {
-        const data = await gyroModel.findByIdAndDelete(id);
-        if (!data) {
-            return res.status(404).json({ message: "GyroData not found" });
-        }
-        res.status(201).json({ message: "Data Deleted Successfully" });
+        const data = await gyroModel.findByIdAndDelete(id)
+            .then(() => res.status(201).json({ message: "Data Deleted Successfully" }))
     } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: "Error deleting GyroData", error: error.message });
+        console.log(error);
     }
-});
+})
 
-// Optional: Function to handle WebSocket messages
-const handleWebSocketMessage = async (data) => {
-    try {
-        const { vehicleNo } = data;
-
-        // Create a new instance of gyroModel from the incoming data
-        const carData = await carModel.findOne({ vehicleNo });
-
-        const gyroPostData = new gyroModel({
-            ...data,
-            car: carData ? carData._id : null
-        });
-
-        await gyroPostData.save();
-
-        return { success: true };
-    } catch (error) {
-        console.error(error);
-        return { success: false, error: error.message };
-    }
-};
-
-// Export the router and the WebSocket handler
-module.exports = { router, handleWebSocketMessage };
 module.exports = router;
+
+// const router = require("express").Router();
+// const gyroModel = require("../models/gyro");
+// const carModel = require("../models/car");
+
+// // POST REQ
+// router.post("/addGyro", async (req, res) => {
+//     try {
+//         const { vehicleNo, GPSfix, Date, Time, Latitude, LatitudeDirection, Longitude, LongitudeDirection, Speed, Heading, NoOfSatellites, Altitude, PDOP, HDOP, NetworkOperatorName, Ignition, MainPowerStatus, MainInputVoltage, EmergencyStatus, GSMSignalStrength, MCC, MNC, LAC, CellId, NMR, DigitalInputStatus, DigitalOutputStatus, acceloX, acceloY, acceloZ, gyroX, gyroY, gyroZ, temperature } = req.body;
+
+//         const carData = await carModel.findOne({ vehicleNo });
+
+//         const gyroPostData = new gyroModel({
+//             vehicleNo,
+//             GPSfix,
+//             Date,
+//             Time,
+//             Latitude,
+//             LatitudeDirection,
+//             Longitude,
+//             LongitudeDirection,
+//             Speed,
+//             Heading,
+//             NoOfSatellites,
+//             Altitude,
+//             PDOP,
+//             HDOP,
+//             NetworkOperatorName,
+//             Ignition,
+//             MainPowerStatus,
+//             MainInputVoltage,
+//             EmergencyStatus,
+//             GSMSignalStrength,
+//             MCC,
+//             MNC,
+//             LAC,
+//             CellId,
+//             NMR,
+//             DigitalInputStatus,
+//             DigitalOutputStatus,
+//             acceloX,
+//             acceloY,
+//             acceloZ,
+//             gyroX,
+//             gyroY,
+//             gyroZ,
+//             temperature,
+//             car: carData ? carData._id : null
+//         });
+
+//         await gyroPostData.save();
+//         res.status(200).json({ message: "GyroData Added Successfully" });
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).json({ message: "Error adding GyroData", error: error.message });
+//     }
+// });
+
+// // GET REQ
+// router.get("/fetchGyro", async (req, res) => {
+//     try {
+//         const data = await gyroModel.find();
+//         res.status(200).json({ data });
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).json({ message: "Error fetching GyroData", error: error.message });
+//     }
+// });
+
+// // GET REQ WITH ID
+// router.get("/fetchGyro/id", async (req, res) => {
+//     const id = req.query.id;
+//     try {
+//         const data = await gyroModel.findById(id);
+//         if (!data) {
+//             return res.status(404).json({ message: "GyroData not found" });
+//         }
+//         res.status(200).json({ data });
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).json({ message: "Error fetching GyroData by ID", error: error.message });
+//     }
+// });
+
+// // UPDATE GYRO BY ID
+// router.patch("/updateGyro/id", async (req, res) => {
+//     const id = req.query.id;
+//     try {
+//         const data = await gyroModel.findByIdAndUpdate(id, req.body);
+//         if (!data) {
+//             return res.status(404).json({ message: "GyroData not found" });
+//         }
+//         res.json({ message: "Data Updated Successfully" });
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).json({ message: "Error updating GyroData", error: error.message });
+//     }
+// });
+
+// // DELETE GYRO BY ID 
+// router.delete("/deleteGyro/id", async (req, res) => {
+//     const id = req.query.id;
+//     try {
+//         const data = await gyroModel.findByIdAndDelete(id);
+//         if (!data) {
+//             return res.status(404).json({ message: "GyroData not found" });
+//         }
+//         res.status(201).json({ message: "Data Deleted Successfully" });
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).json({ message: "Error deleting GyroData", error: error.message });
+//     }
+// });
+
+// // Optional: Function to handle WebSocket messages
+// const handleWebSocketMessage = async (data) => {
+//     try {
+//         const { vehicleNo } = data;
+
+//         // Create a new instance of gyroModel from the incoming data
+//         const carData = await carModel.findOne({ vehicleNo });
+
+//         const gyroPostData = new gyroModel({
+//             ...data,
+//             car: carData ? carData._id : null
+//         });
+
+//         await gyroPostData.save();
+
+//         return { success: true };
+//     } catch (error) {
+//         console.error(error);
+//         return { success: false, error: error.message };
+//     }
+// };
+
+// // Export the router and the WebSocket handler
+// module.exports = { router, handleWebSocketMessage };
+// module.exports = router;
